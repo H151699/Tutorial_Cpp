078a5ce8137372d52b2ca3b93358e6a6d02a3c30
Andrew Jianyou Day <andrewj.day@hotmail.com>
2018-01-23 12:24:15 +0100

    first c++ class

---
+++ Object Oriented Coding/Classes/Cat.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef CAT_H_
+#define CAT_H_
+
+
+
+class Cat {
+	
+public:
+	void speak();
+	void jump();
+
+};
+
+
+
+
+#endif
---
+++ Object Oriented Coding/Classes/Classes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+#include "Cat.h"
+
+int main() {
+
+
+	Cat cat;
+
+	cat.speak();
+	cat.jump();
+
+
+
+
+	cin.get();
+	return 0;
+
+}
